b211b465c23f550dac82a20b465f150c66236c93
kennzuki <kingsyken@gmail.com>
2024-12-06 02:57:48 +0300

    mapping to fetch and display todo's

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import supabase from "./supabaseConfig";
+import supabase from './supabaseConfig';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
@@ -8,27 +8,48 @@ function App() {
   const addTodo = async () => {
     const newTodoData = {
       title: newTodo,
-      IsCompleted:false
+      is_completed: false,
     };
-    const { data, error } = await supabase.from('TodoList').insert([newTodoData]).single()
+    const { data, error } = await supabase
+      .from('TodoList')
+      .insert([newTodoData])
+      .single();
     if (error) {
-      console.log('error in adding todo list');
-      
+      console.log('error in adding todo list', error);
     } else {
-      setTodoList((prev)=>[...prev,data])
+      setTodoList((prev) => [...prev, data]);
+      setNewTodo('');
     }
-    setNewTodo('');
   };
 
   return (
-    <div className="flex flex-col items-center gap-6">
+    <div className='flex flex-col items-center gap-6 mt-20'>
       <h1 className='underline text-xl uppercase'>supabase</h1>
       <section className='mt-8'>
-        <input type='text' className='rounded p-2 border-2' placeholder='Add new todo' />
+        <input
+          type='text'
+          className='rounded p-2 border-2'
+          placeholder='Add new todo'
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
         <button onClick={addTodo} className='py-2 px-4 text-white bg-blue-500'>
           Add Todo Item
         </button>
+       
       </section>
+      <ul className="flex gap-3">
+        {todoList.map((todo) => (
+          <li>
+            <p> {todo.title}</p>
+            <button className='py-2 px-4 text-white bg-green-500'>
+              {' '}
+              {todo.isCompleted ? 'Undo' : 'Complete Task'}
+            </button>
+            <button className='py-2 px-4 text-white bg-red-500' > Delete Task</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
